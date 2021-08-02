d33dd643f9f2a77e594297c6fdc8bca14ef321ad
Bob Pakhriev <boburpakhriev@gmail.com>
2021-08-02 11:36:52 +0900

    Initial commit of context

---
+++ src/components/BookList.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import { ThemeContext } from "../contexts/ThemeContext";
+
+class BookList extends Component {
+  render() {
+    return (
+      <ThemeContext.Consumer>
+        {(context) => {
+          const { isLightTheme, light, dark } = context;
+          const theme = isLightTheme ? light : dark;
+          return (
+            <div
+              className="book-list"
+              style={{ color: theme.syntax, background: theme.bg }}
+            >
+              <ul>
+                <li style={{ background: theme.ui }}>The Power of Habit</li>
+                <li style={{ background: theme.ui }}>Sapiens</li>
+                <li style={{ background: theme.ui }}>Homo Deus</li>
+              </ul>
+            </div>
+          );
+        }}
+      </ThemeContext.Consumer>
+    );
+  }
+}
+
+export default BookList;
